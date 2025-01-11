aa1bef3c33994abd3a7c620ebdce0723a3986728
DamiaoTwT <920418653@qq.com>
2025-01-11 21:40:16 +0800

    UPDATE ENTITY-PRODUCT

--- src/main/java/com/example/mallxx/entity/Product.java
+++ src/main/java/com/example/mallxx/entity/Product.java
@@ -1,9 +1,15 @@
 package com.example.mallxx.entity;
 
 public class Product {
+
     private int id;
     private String name;
+    private String category;
     private String img_url;
+    private String description;
+    private double price;
+    private int stock;
+
 
     public int getId() {
         return id;
@@ -21,11 +27,56 @@ public class Product {
         this.name = name;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public String getImg_url() {
         return img_url;
     }
 
     public void setImg_url(String img_url) {
         this.img_url = img_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", img_url='" + img_url + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                '}';
     }
 }
