4959a58df69f03f94d60a4df4f4729151e397612
mazy11 <mazy11@asiainfo.com>
2025-01-17 00:15:54 +0800

    mazy购物车

--- src/main/java/com/example/mallxx/controller/Shopping_cartsController.java
+++ src/main/java/com/example/mallxx/controller/Shopping_cartsController.java
@@ -21,6 +21,43 @@ public class Shopping_cartsController {
         this.shopping_cartsMapper = shopping_cartsMapper;
     }
 
+    /**
+     * 通过user_id获取family_id，再通过family_id获取所有user_id,再获取所有user_id的购物车信息
+     * @param user_id
+     */
+    /*@PostMapping("/getCart")
+    public ResponseEntity<List<Shopping_carts>> getCart(@CookieValue(value = "user_id", required = false)String user_id) {
+        int id=Integer.parseInt(user_id);
+
+        Shopping_carts shopping_carts=shopping_cartsMapper.findByUser_id(id);
+        System.out.println(shopping_carts);
+        return ResponseEntity.ok(shopping_cartsMapper.findAll());
+    }*/
+
+    /**
+     * findByUser_id2
+     * 通过user_id获取购物车商品信息
+     * @param user_id
+     */
+    @PostMapping("/getCart2")
+    public ResponseEntity<List<Shopping_carts>> getCart2(@CookieValue(value = "user_id", required = false)String user_id) {
+        int id=Integer.parseInt(user_id);
+        System.out.println(id);
+        return ResponseEntity.ok(shopping_cartsMapper.findByUser_id2(id));
+    }
+
+    /**
+     * findByUser_id3
+     * 通过user_id获取家庭购物车
+     * @param user_id
+     */
+    @PostMapping("/getCart3")
+    public ResponseEntity<List<Shopping_carts>> getCart3(@CookieValue(value = "user_id", required = false)String user_id) {
+        int id=Integer.parseInt(user_id);
+        System.out.println(id);
+        return ResponseEntity.ok(shopping_cartsMapper.findByUser_id3(id));
+    }
+
     //成功
     @PostMapping("/InsertShopping_carts")
     public void AddShopping_carts(@CookieValue(value = "user_id", required = false)String user_id) {
--- src/main/java/com/example/mallxx/mapper/Shopping_cartsMapper.java
+++ src/main/java/com/example/mallxx/mapper/Shopping_cartsMapper.java
@@ -12,4 +12,86 @@ public interface Shopping_cartsMapper {
 
     @Select("select * from Shopping_carts where user_id=#{user_id}")
     Shopping_carts findByUser_id(@Param("user_id") int user_id);
+
+    /**
+     * SELECT
+     *     cd.cart_details_id,
+     *     sc.cart_id,
+     *     cd.product_id,
+     *     cd.quantity,
+     *     cd.unit_price,
+     *     cd.merchant_id
+     * FROM
+     *     shopping_carts AS sc
+     * JOIN
+     *     cart_details AS cd ON sc.cart_id = cd.cart_id
+     * WHERE
+     *     sc.user_id = 1; -- 这里用?表示参数位置，实际执行时需要替换为具体的user_id值
+     */
+    @Select("SELECT " +
+            "    cd.cart_details_id, " +
+            "    sc.cart_id, " +
+            "    cd.product_id, " +
+            "    cd.quantity, " +
+            "    cd.unit_price, " +
+            "    cd.merchant_id " +
+            "FROM " +
+            "    shopping_carts AS sc " +
+            "JOIN " +
+            "    cart_details AS cd ON sc.cart_id = cd.cart_id " +
+            "WHERE " +
+            "    sc.user_id = #{user_id} "
+    )
+    List<Shopping_carts> findByUser_id2(@Param("user_id") int user_id);
+
+    /**
+     * SELECT
+     *     u.user_id,
+     *     u.username,
+     *     cd.cart_details_id,
+     *     sc.cart_id,
+     *     p.id AS product_id,
+     *     cd.quantity,
+     *     cd.unit_price,
+     *     cd.merchant_id,
+     *     p.name AS product_name,
+     *     p.img_url
+     * FROM
+     *     users u
+     * JOIN
+     *     shopping_carts sc ON u.user_id = sc.user_id
+     * JOIN
+     *     cart_details cd ON sc.cart_id = cd.cart_id
+     * JOIN
+     *     products p ON cd.product_id = p.id
+     * WHERE
+     *     u.family_id = (SELECT family_id FROM users WHERE user_id = 1) -- 这里用?表示参数位置，实际执行时需要替换为具体的user_id值
+     * ORDER BY
+     *     u.user_id, sc.cart_id, cd.cart_details_id;
+     */
+    @Select("SELECT " +
+            "    u.user_id, " +
+            "    u.username, " +
+            "    cd.cart_details_id, " +
+            "    sc.cart_id, " +
+            "    p.id AS product_id, " +
+            "    cd.quantity, " +
+            "    cd.unit_price, " +
+            "    cd.merchant_id, " +
+            "    p.name AS product_name, " +
+            "    p.img_url " +
+            "FROM " +
+            "    users u " +
+            "JOIN " +
+            "    shopping_carts sc ON u.user_id = sc.user_id " +
+            "JOIN " +
+            "    cart_details cd ON sc.cart_id = cd.cart_id " +
+            "JOIN " +
+            "    products p ON cd.product_id = p.id " +
+            "WHERE " +
+            "    u.family_id = (SELECT family_id FROM usersWHERE user_id = #{user_id}) " +
+            "ORDER BY " +
+            "    u.user_id, sc.cart_id, cd.cart_details_id;"
+            )
+    List<Shopping_carts> findByUser_id3(@Param("user_id") int user_id);
 }
