1d97e6247a340987de829d8596ebe87d72ba9d65
DamiaoTwT <920418653@qq.com>
2025-01-16 19:56:42 +0800

    ryh

---
+++ src/main/java/com/example/mallxx/entity/Product_exam.java
@@ -0,0 +1,22 @@
+package com.example.mallxx.entity;
+
+public class Product_exam {
+    private int id;
+    private int exam;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getExam() {
+        return exam;
+    }
+
+    public void setExam(int exam) {
+        this.exam = exam;
+    }
+}
