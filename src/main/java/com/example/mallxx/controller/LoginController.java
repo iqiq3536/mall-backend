2aa13232a886abe86272319acecbc1130ac6d16e
mazy11 <mazy11@asiainfo.com>
2025-01-16 16:22:54 +0800

    mazy登陆

--- src/main/java/com/example/mallxx/controller/LoginController.java
+++ src/main/java/com/example/mallxx/controller/LoginController.java
@@ -4,6 +4,9 @@ import com.example.mallxx.mapper.AdminMapper;
 import com.example.mallxx.mapper.MerchantMapper;
 import com.example.mallxx.mapper.SellerMapper;
 import com.example.mallxx.mapper.UserMapper;
+import com.example.mallxx.entity.Merchant;
+import com.example.mallxx.entity.User;
+import com.example.mallxx.entity.Admin;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
@@ -32,8 +35,10 @@ public class LoginController {
         String password = loginData.get("password");
         System.out.println(username + " " + password);
         Map<String, Object> response = new HashMap<>();
-        if (userMapper.loginUser(username, password) != null) {
-            Cookie userIdCookie = new Cookie("userId", username);
+        User user = userMapper.loginUser(username, password);
+        if (user != null) {
+            int user_id = user.getUser_id();
+            Cookie userIdCookie = new Cookie("user_id", String.valueOf(user_id));
             userIdCookie.setHttpOnly(true);
             userIdCookie.setMaxAge(3600);
             userIdCookie.setPath("/");
@@ -55,8 +60,9 @@ public class LoginController {
         String password = loginData.get("password");
         System.out.println(username + " " + password);
         Map<String, Object> response = new HashMap<>();
-        if (!merchantMapper.findByUsernameAndPassword(username, password).isEmpty()) {
-            Cookie userIdCookie = new Cookie("merchantId", username);
+        Merchant merchant = merchantMapper.findByUsernameAndPassword(username, password).get(0);
+        if ( merchant != null) {
+            Cookie userIdCookie = new Cookie("merchantId", String.valueOf(merchant.getId()));
             userIdCookie.setHttpOnly(true);
             userIdCookie.setMaxAge(3600);
             userIdCookie.setPath("/");
@@ -64,7 +70,8 @@ public class LoginController {
             System.out.println("1");
             response.put("success", true);
             response.put("message", "登录成功！");
-        } else {
+        }
+      else {
             System.out.println("2");
             response.put("success", false);
             response.put("message", "登录失败：用户名或密码错误");
@@ -78,8 +85,9 @@ public class LoginController {
         String password = loginData.get("password");
         System.out.println(username + " " + password);
         Map<String, Object> response = new HashMap<>();
-        if (adminMapper.loginAdmin(username, password) != null) {
-            Cookie userIdCookie = new Cookie("adminId", username);
+        Admin admin = adminMapper.loginAdmin(username, password);
+        if ( admin != null) {
+            Cookie userIdCookie = new Cookie("adminId", String.valueOf(admin.getAdminId()));
             userIdCookie.setHttpOnly(true);
             userIdCookie.setMaxAge(3600);
             userIdCookie.setPath("/");
